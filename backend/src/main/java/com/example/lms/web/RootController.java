2ebb91619fa6e233dcfd10a63069c740603e7ab0
123santhosh2999 <rsanthosh2999@gmail.com>
2026-02-04 16:53:21 +0530

    updated package

---
+++ backend/src/main/java/com/example/lms/web/RootController.java
@@ -0,0 +1,14 @@
+package com.example.lms.web;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+
+  @GetMapping(value = "/", produces = MediaType.TEXT_PLAIN_VALUE)
+  public String root() {
+    return "LMS backend is running";
+  }
+}
